9e0e7687372e355fc94edcbc627141de74d815cd
Your Name <your.email@example.com>
2025-10-22 09:39:11 +0530

    Set production port to 10000

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
